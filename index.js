e364505e9e7f506e26ba41e896b7b66bcaea114c
Tavonga Tawonezvi <ttawonezvi13@gmail.com>
2022-10-27 20:30:24 +0200

    final api ticks

--- index.js
+++ index.js
@@ -8,39 +8,37 @@ function dateOnChange(){
     
     let date = day + ' ' + month;
     console.log(date);
+    fetchAsync(date)
 }
 
-
-fetchAsync();
-async function doFetch() {
+async function doFetch(date) {
     const rsp = await fetch(
-      "https://en.wikipedia.org/w/rest.php/v1/search/page?q=17 Mar&limit=10",
+      "https://en.wikipedia.org/w/rest.php/v1/search/page?q="+date+"&limit=10",
      {'Api-User-Agent': 'MediaWiki REST API docs examples/0.1 (https://www.mediawiki.org/wiki/API_talk:REST_API)'}
     );
     const data = await rsp.json();
     return data;
   }
   
-  async function fetchAsync()
+  async function fetchAsync(date)
   {
     try {
-      let result = await doFetch();
+      let result = await doFetch(date);
       let content = document.getElementById("content");
+      content.innerHTML = "";
       console.log(result);
-
+      
       for(let i = 0; i< result.pages.length; i++){
           if(result.pages[i].thumbnail === null){
-          //  content.innerHTML += "<div class='row w-50 list-item'> <div class='col img-wrap'>  <div class='bg-image hover-overlay ripple shadow-2-strong rounded-5'  data-mdb-ripple-color='light'> <img src='img/dog.jpg' style='width:280px; height:199px' class='img-fluid' />  <a href='#!'> <div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div> </a>  </div>  </div> <div class='col '>  <span class='badge bg-danger px-2 py-1 shadow-1-strong mb-3'>News of the day</span> <h4><strong>"+result.pages[i].title+"</strong></h4>  <p class='text-muted'>  "+result.pages[i].excerpt+"  </p>  <button type='button' class='btn btn-primary'>READ MORE</button>  </div>  </div>"
-            continue;
-          }
-                                                                  //-md-6 mb-4                                                                                                                                                                                                                                                                                              //-md-6 mb-4                                                  
-       content.innerHTML += "<div class='row w-50  list-item'> <div class='col img-wrap'>  <div class='bg-image hover-overlay ripple shadow-2-strong rounded-5'  data-mdb-ripple-color='light'> <img src="+result.pages[i].thumbnail.url+" style='width:280px; height:199px' class='img-fluid' />  <a href='#!'> <div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div> </a>  </div>  </div> <div class='col'>  <span class='badge bg-danger px-2 py-1 shadow-1-strong mb-3'>News of the day</span> <h4><strong>"+result.pages[i].title+"</strong></h4>  <p class='text-muted'>  "+result.pages[i].excerpt+"  </p>  <button type='button' class='btn btn-primary'>READ MORE</button>  </div>  </div>"
+             content.innerHTML += "<div class='row w-50 list-item'> <div class='col img-wrap'>  <div class='bg-image hover-overlay ripple shadow-2-strong rounded-5'  data-mdb-ripple-color='light'> <img src='img/dog.jpg' style='width:280px; height:199px' class='img-fluid' />  <a href='#!'> <div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div> </a>  </div>  </div> <div class='col '>  <span class='badge bg-danger px-2 py-1 shadow-1-strong mb-3'>News of the day</span> <h4><strong>"+result.pages[i].title+"</strong></h4>  <p class='text-muted'>  "+result.pages[i].excerpt+"  </p>  <button type='button' class='btn btn-primary'>READ MORE</button>  </div>  </div>"
+          continue;
+          }                                                                       //-md-6 mb-4                                                                                                                                                                                                                                                                                                                                      //-md-6 mb-4                                                  
+       content.innerHTML += "<div class='row w-50 list-item'> <div class='col img-wrap'>  <div class='bg-image hover-overlay ripple shadow-2-strong rounded-5'  data-mdb-ripple-color='light'> <img src="+result.pages[i].thumbnail.url+" style='width:280px; height:199px' class='img-fluid' />  <a href='#!'> <div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div> </a>  </div>  </div> <div class='col'>  <span class='badge bg-danger px-2 py-1 shadow-1-strong mb-3'>News of the day</span> <h4><strong>"+result.pages[i].title+"</strong></h4>  <p class='text-muted'>  "+result.pages[i].excerpt+"  </p>  <button type='button' class='btn btn-primary'>READ MORE</button>  </div>  </div>"
       }
 
     } catch( err ) {
       console.error( err.message );
     }
   }
-let searchDate = document.getElementById("search-item");
-let searchButton = document.getElementById("button-addon1");
+
 
